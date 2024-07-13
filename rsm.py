3ac712baeb5955a65c7b409ff1e6af06d3551da1
Ewan Jordan <ewan.jordan30@gmail.com>
2024-07-13 11:15:45 -0400

    f

--- rsm.py
+++ rsm.py
@@ -33,6 +33,6 @@ model.rgb_features[0].register_forward_hook(get_activation(LAYER))
 
 data_loader = DataLoader(params.TEST_PATH, params.BATCH_SIZE, params.TRAIN_VAL_SPLIT)
 for i, (img, cls_map, label) in enumerate(data_loader.load_batch()):
-    model(img)
+    model(img, is_grasp=False)
     print(activation[LAYER])
     break
