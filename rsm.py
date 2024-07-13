0e6a8e766a3b35d2a007717ba2d9f38f36295f41
Ewan Jordan <ewan.jordan30@gmail.com>
2024-07-13 14:58:21 -0400

    rsm

--- rsm.py
+++ rsm.py
@@ -77,7 +77,7 @@ activations = [[],[],[],[],[]]
 data_loader = DataLoader(params.TEST_PATH, params.BATCH_SIZE, params.TRAIN_VAL_SPLIT)
 labels = data_loader.get_cls_id()
 for i, (img, cls_map, label) in enumerate(data_loader.load_cls()):
-    activations[label.item()].append(model.rgb_features[0](img))
+    activations[label.item()].append(model.rgb_features[0](img[:, :3, :, :]))
 activations_flat = []
 for i in range(5):
     for act in activations[i]:
