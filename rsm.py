34a6b078f2e1512ce6db96d4e1e74c2949ba4229
Ewan Jordan <ewan.jordan30@gmail.com>
2024-07-13 11:37:29 -0400

    check length

--- rsm.py
+++ rsm.py
@@ -34,4 +34,5 @@ model.rgb_features[0].register_forward_hook(get_activation(LAYER))
 data_loader = DataLoader(params.TEST_PATH, params.BATCH_SIZE, params.TRAIN_VAL_SPLIT)
 for i, (img, cls_map, label) in enumerate(data_loader.load_batch()):
     model(img, is_grasp=False)
-    print(label)
+    print(len(img))
+    print(len(label))
