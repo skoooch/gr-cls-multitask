426869a5afea8c66e7a21e29a1fd8babf31bb58a
skoooch <ewan.jordan30@gmail.com>
2024-11-14 00:43:42 -0500

    update for shuffled dataset functionality

--- multi_test.py
+++ multi_test.py
@@ -26,7 +26,7 @@ accuracy, loss = get_test_acc(model)
 import torch
 import os
 import time
-
+import sys
 from utils.parameters import Params
 from multi_task_models.grcn_multi_alex import Multi_AlexnetMap_v3
 from training.single_task.evaluation import get_cls_acc, get_grasp_acc, visualize_grasp, visualize_cls
@@ -36,15 +36,18 @@ params = Params()
 model_name = params.MODEL_NAME
 weights_dir = params.MODEL_PATH
 weights_path = os.path.join(weights_dir, model_name, model_name + '_final.pth')
-
 # AlexNet with 1st, 2nd layer pretrained on Imagenet
 model =  Multi_AlexnetMap_v3().to('cuda')
 model.load_state_dict(torch.load(weights_path))
 model.eval()
+path = params.TEST_PATH
+if len(sys.argv) > 1:
+    path = params.TEST_PATH_SHUFFLE
+
 # Get test acc for CLS model
-c_accuracy, c_loss = get_cls_acc(model, include_depth=True, seed=None, dataset=params.TEST_PATH, truncation=None)
+c_accuracy, c_loss = get_cls_acc(model, include_depth=True, seed=None, dataset=path, truncation=None)
 # Get test acc for Grasp model
-accuracy, loss = get_grasp_acc(model, include_depth=True, seed=None, dataset=params.TEST_PATH, truncation=None)
+accuracy, loss = get_grasp_acc(model, include_depth=True, seed=None, dataset=path, truncation=None)
 
 print('Grasp: %s' % accuracy, loss)
 print('CLS: %s' % c_accuracy, c_loss)
