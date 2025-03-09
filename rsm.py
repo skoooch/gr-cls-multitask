931913b6d969dc2e73d85c4bf3fd792da1fd36e8
skoooch <ewan.jordan30@gmail.com>
2025-03-08 19:06:40 -0500

    Update RSA code

--- rsm.py
+++ rsm.py
@@ -127,7 +127,7 @@ act_array = get_feature_activations(model, images, labels, layer_i=1)
 for i in [1, 5, 8, 11]:
     act_array = get_feature_activations(model, images, labels, layer_i=i)
     result = squareform(pdist(act_array, metric="correlation"))
-    np.save("saved_model_rsms/features_%s.npy" % (i-1), result)
+    np.save("saved_model_rsms/grasp/features_%s.npy" % (i-1), result)
 
 num_images_per_label = 5
 # embedding = MDS.cmdscale(result, 2)[0]
