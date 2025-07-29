2c81082eb28c55f2805bdbd9aaf51eeb22bcd090
skoooch <ewan.jordan30@gmail.com>
2025-07-29 16:49:54 -0400

    Updated repo - added depth shapley scores

--- goodale/rectangle_dataset/visualize_data.py
+++ goodale/rectangle_dataset/visualize_data.py
@@ -8,10 +8,7 @@ def generate_video(img):
     for i in range(90):
         p = np.array(img[89 - i, i//2, 0, 0, :, :, :], dtype = np.uint8)
         plt.imsave("file%02d.png" % i, p)
-    subprocess.call([
-        'ffmpeg', '-framerate', '8', '-i', 'file%02d.png', '-r', '30', '-pix_fmt', 'yuv420p',
-        'video_name2.mp4'
-    ])
+    subprocess.call(['ffmpeg', '-framerate', '8', '-i', '%2d.png', '-r', '30', '-pix_fmt', 'yuv420p','video_name2.mp4'])
     for file_name in glob.glob("*.png"):
         os.remove(file_name)
 output_dir = '/scratch/expires-2024-Dec-14/'
--- multi_task_models/grcn_multi_alex.py
+++ multi_task_models/grcn_multi_alex.py
@@ -129,6 +129,7 @@ class Multi_AlexnetMap_v3(nn.Module):
         rgb = x[:, :3, :, :]
         d = torch.unsqueeze(x[:, 3, :, :], dim=1)
         d = torch.cat((d, d, d), dim=1)
+        # d = torch.zeros(d.shape).to("cuda")
         # First Layer Shapley!!!!! -------------------------------------------------------
         # if shap_mask != []:
             
@@ -153,14 +154,23 @@ class Multi_AlexnetMap_v3(nn.Module):
         d = self.d_features(d)
         x = torch.cat((rgb, d), dim=1)
         if shap_mask != []:
-            features_conv_out = self.features[0:11](x)
             shap_mask_idx = shap_mask.bool()  # Convert to a boolean mask for indexing
             # Broadcast activations to match the shape of the output
             broadcasted_activations = activations.unsqueeze(0)  # Add batch dimension
-            broadcasted_activations = broadcasted_activations.expand_as(features_conv_out)  # Expand to match rgb_conv_out's shape
+            broadcasted_activations = broadcasted_activations.expand_as(x)  # Expand to match rgb_conv_out's shape
             # Set rgb_conv_out where shap_mask is True
-            features_conv_out[:, shap_mask_idx, :, :] = broadcasted_activations[:, shap_mask_idx, :, :]
-            x = self.features[11:](features_conv_out)
+            x[:, shap_mask_idx, :, :] = broadcasted_activations[:, shap_mask_idx, :, :]
+            for i in range(len(self.features)):
+                x = self.features[i](x)
+            #------------shapley non first layer ----------
+            # features_conv_out = self.features[0:11](x)
+            # shap_mask_idx = shap_mask.bool()  # Convert to a boolean mask for indexing
+            # # Broadcast activations to match the shape of the output
+            # broadcasted_activations = activations.unsqueeze(0)  # Add batch dimension
+            # broadcasted_activations = broadcasted_activations.expand_as(features_conv_out)  # Expand to match rgb_conv_out's shape
+            # # Set rgb_conv_out where shap_mask is True
+            # features_conv_out[:, shap_mask_idx, :, :] = broadcasted_activations[:, shap_mask_idx, :, :]
+            # x = self.features[11:](features_conv_out)
         elif dissociate != []:
             prev = 0
             for index, i in enumerate([1,5,8,11]):
