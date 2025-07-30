64f3233febfdae466004ddc818cf9a79dd13764e
skoooch <ewan.jordan30@gmail.com>
2025-07-30 18:25:05 -0400

    add shap data

--- process_data.py
+++ process_data.py
@@ -1,3 +1,4 @@
+from os import remove
 import numpy as np
 from scipy.signal import butter, sosfilt, filtfilt
 import scipy.io
@@ -42,9 +43,8 @@ def get_data(task, avr=False):
             all_data[category].append(averaged_trials)
     return all_data
 
-def get_data_matlab(task,avr=False, left = False):
+def get_data_matlab(task,avr=True, left = False):
     categories = ['figurine', 'pen', 'chair', 'lamp', 'plant']
-    all_data = {}
     dataset = []
     if task == "grasp":
         removed_participants = [2,11,7,3,10,12,13] # add more participants as needed
@@ -52,37 +52,63 @@ def get_data_matlab(task,avr=False, left = False):
     else:
         removed_participants = [1,10,12,13, 16]
         #removed_participants = [1,2,11,7,3,10,12,13]
+    all_data = {}
+    if not avr:
+        all_data = []
+        
     for i in range(1,17):
         if i not in removed_participants:
             mat = scipy.io.loadmat(f'matlab_files/{task}_erps/kirtan_exp_{i}_{task}.mat')
             dataset.append(mat)
-    for category in categories:
-        all_data[category] = []
-        for i in range(1, 6):
-            object_to_average_over_exp = []
-            for file in dataset:
-                data = file[category][f"ob{i}"][0][0]
-                data = data.transpose(0, 2, 1)
-                num_trials, num_timepoints, num_channels = data.shape
-                if(np.where(data) == 0):
-                    print("here")
-                # Determine the number of new trials after averaging every 4
-                # Initialize the array to hold the averaged data
-                averaged_trials = np.mean(data, axis=0)[None, :, :]
-                # Append data
-                object_to_average_over_exp.append(averaged_trials)
-            concat_begin = object_to_average_over_exp[0]
-            for j in range(1, len(object_to_average_over_exp)):
-                concat_begin = np.concatenate((concat_begin, object_to_average_over_exp[j]), axis=0)
-            summed = concat_begin.sum(axis=0)/len(object_to_average_over_exp)
-            # 20:33 + 56: is all the back
-            
-            if not left: summed = np.concatenate((summed[:, 20:33], summed[:, 56:]), axis=1)
-            else: summed = summed[:, 20:33]
-            assert(summed.shape[0] == 307)
-            all_data[category].append(summed)
-    return all_data
+            if not avr: all_data.append({})
+    if avr:
+        for category in categories:
+            all_data[category] = []
+            for i in range(1, 6):
+                object_to_average_over_exp = []
+                for p, file in enumerate(dataset):
+                    data = file[category][f"ob{i}"][0][0]
+                    data = data.transpose(0, 2, 1)
+                    num_trials, num_timepoints, num_channels = data.shape
+                    if(np.where(data) == 0):
+                        print("here")
+                    # Determine the number of new trials after averaging every 4
+                    # Initialize the array to hold the averaged data
+                    averaged_trials = np.mean(data, axis=0)[None, :, :]
+                    # Append data
+                    object_to_average_over_exp.append(averaged_trials)
+                concat_begin = object_to_average_over_exp[0]
+                for j in range(1, len(object_to_average_over_exp)):
+                    concat_begin = np.concatenate((concat_begin, object_to_average_over_exp[j]), axis=0)
+                summed = concat_begin.sum(axis=0)/len(object_to_average_over_exp)
+                # 20:33 + 56: is all the back
+                
+                if not left: summed = np.concatenate((summed[:, 20:33], summed[:, 56:]), axis=1)
+                else: summed = summed[:, 20:33]
+                assert(summed.shape[0] == 307)
+                all_data[category].append(summed)
+        return all_data
+    else:
+        for p, file in enumerate(dataset):
+            for category in categories:
+                all_data[p][category] = []
+                for i in range(1, 6):
+                    data = file[category][f"ob{i}"][0][0]
+                    data = data.transpose(0, 2, 1)
+                    num_trials, num_timepoints, num_channels = data.shape
+                    if(np.where(data) == 0):
+                        print("here")
 
+                    # Determine the number of new trials after averaging every 4
+                    # Initialize the array to hold the averaged data
+                    summed = np.mean(data, axis=0)[:, :]
+                                        
+                    if not left: summed = np.concatenate((summed[:, 20:33], summed[:, 56:]), axis=1)
+                    else: summed = summed[:, 20:33]
+                    assert(summed.shape[0] == 307)
+                    all_data[p][category].append(summed)
+        return all_data
+            
 # # this function was just to test whether the new data aligned with the new stuff (it did not...)
 # def test_data():
 #     categories = ['figurine', 'pen', 'chair', 'lamp', 'plant']
