b3210490504481e3974d1d1faa1da167a542092d
Ewan Jordan <ewan.jordan30@gmail.com>
2026-03-01 13:05:07 -0500

    merge fix

--- shap_debug.sh
+++ shap_debug.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=shapley_parallel
 #SBATCH --output=shap_log/shapley_%A_%a.out
 #SBATCH --error=shap_log/shapley_%A_%a.err
-#SBATCH --array=0-4
-#SBATCH --partition=gpunodes
+#SBATCH --array=4
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --time=3-00:00:00
-srun python3 shapley_debug.py cuda grasp ${SLURM_ARRAY_TASK_ID} round1_test
+srun python3 shapley_debug.py cuda cls ${SLURM_ARRAY_TASK_ID} round1_ustc_3
